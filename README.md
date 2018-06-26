7041280336aed0fde4a52493c62704ce7954d9cc
Primo Sabatini <primo.sabatini@gmail.com>
2018-06-26 01:51:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-cli-plugin-tailwindcss
+
+[Vue CLI 3.x](https://github.com/vuejs/vue-cli) plugin to add TailwindCSS to your Vue JS project.
+
+## Getting Started
+
+Create a project with Vue CLI 3:
+```bash
+vue create [my-vue-app]
+```
+
+Add the plugin to your project:
+```bash
+cd my-vue-app
+vue add tailwindcss
+```
+
+## License
+[MIT](http://opensource.org/licenses/MIT)
---
+++ generator/index.js
@@ -0,0 +1,41 @@
+module.exports = (api, options, rootOptions) => {
+    const tailwindPkg = {
+        devDependencies: {
+            "tailwindcss": "^0.6.1"
+        }
+    }
+
+    api.extendPackage(tailwindPkg)
+
+    api.render({
+        "./src/assets/styles/main.css": "./templates/assets/styles/main.css",
+        "./src/tailwind.config.js": "./templates/assets/src/tailwind.config.js"
+    }, opts)
+
+    const postcssConfig = {
+        "plugins": {
+            "tailwindcss": "./tailwind.config.js",
+            "autoprefixer": {}
+        }
+    }
+
+    api.extendPackage({ postcss: postcssConfig })
+
+    api.onCreateComplete(() => {
+        // inject into App.vue
+        const fs = require("fs")
+        const mainPath = api.resolve("./src/main.js")
+
+        // get content
+        let contentMain = fs.readFileSync(mainPath, {encoding: "utf-8"})
+        const lines = contentMain.split(/\r?\n/g).reverse()
+
+        // inject import
+        const lastImportIndex = lines.findIndex((line) => line.match(/^import/))
+        lines[lastImportIndex] += 'import "@/assets/styles/main.css";'
+
+        // write file
+        contentMain = lines.reverse().join("\n")
+        fs.writeFileSync(mainPath, contentMain, {encoding: "utf-8"})
+    })
+}
